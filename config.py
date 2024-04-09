292d526a3cac957f41ee73be35e694af8d3b0f1e
Jalithc <126810658+Jalithc@users.noreply.github.com>
2024-04-09 10:24:01 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,13 @@
+'''Configuration of flask project'''
+
+from flask import Flask, render_template
+
+#assigning FLASK session
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
